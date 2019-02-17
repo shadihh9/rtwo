7d546c14fbdef351585a211725c47c2b6323fdec
shadihh9 <shadihh9@gmail.com>
2019-02-17 22:28:33 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,26 @@
+<h1 align="center">
+	▀▀█▀▀ █░░█ █▀▀ ▀█░█▀ ░▀░ █▀▀█ █▀▀ █▀▀█ █░█ █░█ █▀▀ █░░█
+░░█░░ █▀▀█ █▀▀ ░█▄█░ ▀█▀ █░░█ █▀▀ █▄▄▀ ▄▀▄ ▄▀▄ ▀▀█ █▄▄█
+░░▀░░ ▀░░▀ ▀▀▀ ░░▀░░ ▀▀▀ █▀▀▀ ▀▀▀ ▀░▀▀ ▀░▀ ▀░▀ ▀▀▀ ▄▄▄█
+</h1>
+
+<p align="center">
+Get user information of a Twitter user!
+</p>
+## Install
+
+```
+$ npm install --global rtwo
+```
+__`OR`__
+```
+$ sudo npm install --global rtwo
+```
+
+## Usage
+```
+$ Usage: rtwo <user-name>
+
+  Example:
+    $ rtwo 9gag
+```
---
+++ cli.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const dns = require('dns');
+const got = require('got');
+const cheerio = require('cheerio');
+const logUpdate = require('log-update');
+const ora = require('ora');
+const updateNotifier = require('update-notifier');
+const pkg = require('./package.json');
+
+updateNotifier({pkg}).notify();
+const arg = process.argv[2];
+const spinner = ora();
+
+if (!arg) {
+	console.log(`
+ Usage: rtwo <user-name>
+
+ Example:
+   $ rtwo 9gag
+ `);
+	process.exit(1);
+}
+
+dns.lookup('twitter.com', err => {
+	if (err) {
+		logUpdate(`\n› Please check your internet connection\n`);
+		process.exit(1);
+	} else {
+		logUpdate();
+		spinner.text = `rtwo ${arg}`;
+		spinner.start();
+	}
+});
+
+const url = `https://twitter.com/${arg}`;
+
+got(url).then(res => {
+	const $ = cheerio.load(res.body);
+	logUpdate(`
+› Name      : ${$('.ProfileHeaderCard-nameLink').text()}
+› Handle    : ${$('.ProfileHeaderCard-screennameLink').text().trim()}
+› Biography : ${$('.ProfileHeaderCard-bio').text()}
+› Place     : ${$('.ProfileHeaderCard-locationText').text().trim()}
+› Joined    : ${$('.ProfileHeaderCard-joinDateText').text().trim().replace('Joined', '').trim()}
+› Tweets    : ${$('.ProfileNav-value').eq(0).text().trim()}
+› Following : ${$('.ProfileNav-value').eq(1).text().trim()}
+› Follwers  : ${$('.ProfileNav-value').eq(2).text().trim()}
+› Likes     : ${$('.ProfileNav-value').eq(3).text().trim()}
+ `);
+	spinner.stop();
+}).catch(err => {
+	if (err) {
+		logUpdate(`\n› ${arg} is not a Twitter user\n`);
+		process.exit(1);
+	}
+});
+
